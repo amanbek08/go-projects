48d8e81b221a58b5e708712dea8aa37fb8cddfc8
Amanbek Orynbek <amanbek.orynbek@gmail.com>
2022-09-11 15:33:52 +0600

    Added heapify down

--- structures/MaxHeap.go
+++ structures/MaxHeap.go
@@ -1,5 +1,7 @@
 package structures
 
+import "fmt"
+
 type MaxHeap struct {
 	array []int
 }
@@ -18,6 +20,46 @@ func right(i int) int {
 
 func (h *MaxHeap) swap(i1, i2 int) {
 	h.array[i1], h.array[i2] = h.array[i2], h.array[i1]
+}
+
+func (h *MaxHeap) Extract() int {
+	extracted := h.array[0]
+
+	if len(h.array) == 0 {
+		fmt.Println("Array is empty")
+		return -1
+	}
+
+	h.array[0] = h.array[len(h.array)-1]
+	h.array = h.array[:len(h.array)-1]
+
+	h.MaxHeapifyDown(0)
+
+	return extracted
+}
+
+func (h *MaxHeap) MaxHeapifyDown(index int) {
+	lastIndex := len(h.array) - 1
+	l, r := left(index), right(index)
+	childToCompare := 0
+
+	for l < lastIndex {
+		if l == lastIndex {
+			childToCompare = l
+		} else if h.array[l] > h.array[r] {
+			childToCompare = l
+		} else {
+			childToCompare = r
+		}
+
+		if h.array[index] < h.array[childToCompare] {
+			h.swap(index, childToCompare)
+			index = childToCompare
+			l, r = left(index), right(index)
+		} else {
+			return
+		}
+	}
 }
 
 func (h *MaxHeap) MaxHeapifyUp(index int) {
